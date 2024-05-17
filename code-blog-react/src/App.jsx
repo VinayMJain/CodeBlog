9309775285da49b9c0b8e76be05d02318e740425
Kishan Kumar Vadhia <vadhiakishan@gmail.com>
2024-05-18 02:52:38 +0530

    some updates in routing

--- code-blog-react/src/App.jsx
+++ code-blog-react/src/App.jsx
@@ -1,11 +1,17 @@
 import Home from "./pages/home"
+// import 
 import Navbar from "./components/navbar"
+import { Route, Routes } from "react-router-dom"
 import "./assets/style/root.css"
 function App() {
 
   return (
     <>
-    <Home />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+      {/* <Home /> */}
+    {/* <Home /> */}
     </>
   )
 }
