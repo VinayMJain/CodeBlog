6a2571f31d8282d6218353c91437c21c2ac78341
Kishan Kumar Vadhia <vadhiakishan@gmail.com>
2024-05-18 02:58:03 +0530

    defined routes

--- code-blog-react/src/App.jsx
+++ code-blog-react/src/App.jsx
@@ -1,4 +1,7 @@
 import Home from "./pages/home"
+import Login from "./components/login"
+import Signup from "./components/signup"
+import Upload from "./components/upload"
 // import 
 import Navbar from "./components/navbar"
 import { Route, Routes } from "react-router-dom"
@@ -9,6 +12,9 @@ function App() {
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />}/>
+        <Route path="/signup" element={<Signup />}/>
+        <Route path="/uploads" element={<Upload />}/>
       </Routes>
       {/* <Home /> */}
     {/* <Home /> */}
