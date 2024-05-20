886508ae074063eedd856951a478e596058eaf13
Neel Patel <neelp1478@gmail.com>
2024-05-20 11:45:40 +0530

    hi

--- code-blog-react/src/components/card.jsx
+++ code-blog-react/src/components/card.jsx
@@ -3,20 +3,20 @@ import "../assets/style/card.css"
 import cardImage from "../assets/images/BlogData/binary search.png"
 function Card() {
   return (
-    <div class="card-div">
-        <div class="card">
-            <div class="image"><img src="images/cb-logo-3.webp" alt=""/></div>
-            <div class="topic-name">Binary Search</div>
+    <div className="card-div">
+        <div className="card">
+            <div className="image"><img src="images/cb-logo-3.webp" alt=""/></div>
+            <div className="topic-name">Binary Search</div>
             <div id="explore-button">Explore</div>
         </div>
-        <div class="card">
-            <div class="image"><img src="images/cb-logo-3.webp" alt=""/></div> 
-            <div class="topic-name">Binary Search </div>
+        <div className="card">
+            <div className="image"><img src="images/cb-logo-3.webp" alt=""/></div> 
+            <div className="topic-name">Binary Search </div>
             <div id="explore-button">Explore</div>
         </div>
-        <div class="card">
-            <div class="image"><img src="images/cb-logo-3.webp" alt=""/></div>
-            <div class="topic-name">Binary Search</div>
+        <div className="card">
+            <div className="image"><img src="images/cb-logo-3.webp" alt=""/></div>
+            <div className="topic-name">Binary Search</div>
             <div id="explore-button">Explore</div>
         </div>
     </div>
