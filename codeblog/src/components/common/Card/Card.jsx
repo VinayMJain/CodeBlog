5bbf5ddbe179a242c14c38624d42bfdb319f89ac
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-05-29 12:49:54 +0530

    feat: Add dynamic title to Card component

--- codeblog/src/components/common/Card/Card.jsx
+++ codeblog/src/components/common/Card/Card.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import style from "./Card.module.css";
 
-const Card = () => {
+const Card = (title) => {
   return (
       <div className={style.cardDiv} id="cardDiv">
         <div className={style.card} id="card">
           <div className={style.image} id="cardImage">
-            <img src="images/cb-logo-3.webp" alt="" />
+            <img src="images/cb-logo-3.webp" id="blogImage" alt="" />
           </div>
           <div className={style.topicName} id="topicName">
-            Binary Search
+            {title}
             <button id={style.exploreButton}>explore</button>
           </div>
         </div>
