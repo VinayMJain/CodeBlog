56eefd486e0612f493c73ef553e3f8a66aa03eba
Kishan Kumar Vadhia <vadhiakishan@gmail.com>
2024-05-29 23:49:53 +0530

    made searchbar

--- codeblog/src/App.jsx
+++ codeblog/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Upload from './components/pages/Uploads/Upload'
+import Blogs from './components/pages/Blogs/Blogs'
 
 function App() {
   return (
     <>
-    <h1>CodeBlog</h1>
+    {/* <h1>CodeBlog</h1> */}
+    <Blogs />
     </>
   )
 }
