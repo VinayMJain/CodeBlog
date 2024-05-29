e71c1b4ef32db77865abd509eb268ce17b42f646
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-05-29 14:05:12 +0530

    refactor: Update Card component to use dynamic image source

--- codeblog/src/components/common/Card/Card.jsx
+++ codeblog/src/components/common/Card/Card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import style from "./Card.module.css";
 
-const Card = (title) => {
+const Card = (title,image) => {
   return (
       <div className={style.cardDiv} id="cardDiv">
         <div className={style.card} id="card">
           <div className={style.image} id="cardImage">
-            <img src="images/cb-logo-3.webp" id="blogImage" alt="" />
+            <img src={{image}} id="blogImage" alt="" />
           </div>
           <div className={style.topicName} id="topicName">
             {title}
