68009411f96c1d0aa82ae90a7e04278d7ee70e7e
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-05-29 16:19:33 +0530

    minor change in upload.jsx

--- codeblog/src/components/pages/Uploads/Upload.jsx
+++ codeblog/src/components/pages/Uploads/Upload.jsx
@@ -6,22 +6,20 @@ const blogs = [
   { title: "Quick Sort", path: "images/cb-logo-3.webp" },
   { title: "Binary Search", path: "images/cb-logo-4.webp" },
   { title: "Merge Sort", path: "images/cb-logo-7.webp" },
-  { title: "Quick Sort", path: "images/cb=logo-5.webp" },
+  { title: "Bucket Sort", path: "images/cb=logo-5.webp" },
 ];
 
 const Upload = ({ month }) => {
   return (
     <>
       <div className={style.mainCantainer} id="mainCantainer">
-        <div>
-          <div className={style.pBlog} id="pBlog">
-            <p>{month}</p>
-          </div>
-          <div className={style.uploadBlog} id="uploadBlog">
-            {blogs.map((blog) => (
-              <Card title={blog.title} image={blog.path} />
-            ))}
-          </div>
+        <div className={style.pBlog} id="pBlog">
+          <p id="monthP">{month}</p>
+        </div>
+        <div className={style.uploadBlog} id="uploadBlog">
+          {blogs.map((blog) => (
+            <Card title={blog.title} image={blog.path} />
+          ))}
         </div>
       </div>
     </>
