fc18488bcaa23624f46f5f74bbe1b4bbb5b7918c
Neel Patel <neelp1478@gmail.com>
2024-05-31 15:54:55 +0530

    faq update

---
+++ codeblog/src/components/pages/Faqs/Faqs.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import styles from './Faqs.module.css';
+
+
+function Faqs() {
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const toggleAccordion = (index) => {
+        setActiveIndex(activeIndex === index ? null : index);
+    };
+
+    const faqs = [
+        { question: "What is Lorem Ipsum?", answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." },
+        { question: "Why do we use it?", answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." },
+        { question: "Where does it come from?", answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." },
+        { question: "How do I start blogging?", answer: "Sign up, click \"Create New Post\" on your dashboard, and hit publish when done." },
+        { question: "Can I share posts on social media?", answer: "Yes, you can share your posts on various social media platforms." },
+        { question: "What programming languages can I use?", answer: "You can use a wide range of programming languages depending on your project requirements." },
+        { question: "What programming languages can I use?", answer: "You can use a wide range of programming languages depending on your project requirements." },
+        { question: "How do I contact support?", answer: "You can contact our support team through email or by filling out the contact form on our website." }
+    ];
+
+    return (
+        <>
+            <h1>Frequently Asked Questions</h1>
+            <div className={styles.containerFluid}>
+                {faqs.map((faq, index) => (
+                    <div className={styles.faqSection} key={index}>
+                        <div 
+                            className={`${styles.accordion} ${activeIndex === index ? styles.active : ''}`} 
+                            onClick={() => toggleAccordion(index)}
+                        >
+                            <div className={styles.icon}></div>
+                            <h5>{faq.question}</h5>
+                        </div>
+                        <div 
+                            className={styles.panel} 
+                            style={{ maxHeight: activeIndex === index ? '200px' : '0' }}
+                        >
+                            <p>{faq.answer}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}
+
+export default Faqs;
