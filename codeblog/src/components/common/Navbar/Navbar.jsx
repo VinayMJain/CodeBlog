509acfa63ac59eb4eb241c1f264cf29d057a427d
Archit Agrawal <architagrawal063@gmail.com>
2024-06-01 00:26:10 +0530

    Navbar Updated

--- codeblog/src/components/common/Navbar/Navbar.jsx
+++ codeblog/src/components/common/Navbar/Navbar.jsx
@@ -5,29 +5,88 @@ function Navbar({onPageChange}) {
     const navbarItemArray = ['Blogs','Discussion','Uploads','About','FAQs','Contact']
 
   return (
-    <header className={style.navHeader} id='navHeader'>
-        <nav className={style.navbar} id='navbar'>
-            <div className={style.navbarLogo} id='navbarLogo'>
-                <button className={style.logoButton} id='logoButton'>
-                    <img src="images/logo1.png" alt="logo" id='logo'/>
-                    <span id='CodeBlogName' className={style.CodeBlogName}>CodeBlog</span>
+    <header 
+        className={style.navHeader} 
+        id='navHeaderId'
+        >
+        <nav 
+            className={style.navbar} 
+            id='navbarId'
+            >
+            <div 
+                className={style.navbarLogo} 
+                id='navbarLogoId'
+                >
+                <button 
+                    className={style.logoButton} 
+                    id='logoButtonId'
+                    >
+                    <img 
+                        alt="logo" 
+                        id='logoId'
+                        src="images/logo1.png" 
+                    />
+                    <span 
+                        className={style.CodeBlogName}
+                        id='CodeBlogNameId' 
+                    >CodeBlog
+                    </span>
                 </button>
             </div>
-            <div className={style.navbarOption} id='navbarOption'>
-                <ul className={style.navbarList} id='navbarList'>
+            <div 
+                className={style.navbarOption} 
+                id='navbarOptionId'
+                >
+                <ul 
+                    className={style.navbarList} 
+                    id='navbarListId'
+                    >
                     {navbarItemArray.map((item, index) => {
                         return (
-                            <li key={index} className={style.navbarItem} id={`navbarItem${item}`}>
-                                <button onClick={()=>onPageChange(item)} className={style.navbarLinkButton} id={`navbarLinkButton${item}`}>{item}</button>
+                            <li 
+                                className={style.navbarItem} 
+                                id={`navbarItem${item}`}
+                                key={index} 
+                                >
+                                <button 
+                                    className={style.navbarLinkButton} 
+                                    id={`navbarLinkButton${item}`}
+                                    onClick={()=>onPageChange(item)} 
+                                >{item}
+                                </button>
                             </li>
                         )
                     })}
                 </ul>
             </div>
-            <div className={style.navbarRight} id='navbarRight'>
-                <button className={style.notificationButton}><img src="images/notification.png" alt="Notification" id='notificationLogo'/></button>
-                <div className={style.navbarProfile} id='navbarProfile'>
-                    <button className={style.profileButton} id='profileButton'><img src="images/profile-photo.png" alt="Profile" id='profileLogo'/></button>
+            <div 
+                className={style.navbarRight} 
+                id='navbarRightId'
+                >
+                <button 
+                    className={style.notificationButton}
+                    id='notificationButtonId'
+                >
+                    <img 
+                        alt="Notification" 
+                        id='notificationLogoId'
+                        src="images/notification.png" 
+                    />
+                </button>
+                <div 
+                    className={style.navbarProfile} 
+                    id='navbarProfileId'
+                    >
+                    <button 
+                        className={style.profileButton} 
+                        id='profileButtonId'
+                        >
+                            <img 
+                                alt="Profile" 
+                                id='profileLogoId'
+                                src="images/profile-photo.png" 
+                            />
+                    </button>
                 </div>
             </div>
         </nav>
