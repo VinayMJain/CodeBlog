b282733306277b83133116514c81b2f2c99ec428
Kishan Kumar Vadhia <vadhiakishan@gmail.com>
2024-06-15 02:31:03 +0530

    saving state of the browser

--- codeblog/src/components/common/Navbar/Navbar.jsx
+++ codeblog/src/components/common/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import style from './Navbar.module.css'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import style from './Navbar.module.css';
+import { Link } from 'react-router-dom';
 
 function Navbar({ onPageChange }) {
-    let [currentPage, setCurrentPage] = useState('Blogs');
+    const [currentPage, setCurrentPage] = useState(() => {
+        return sessionStorage.getItem('currentPage') || 'Blogs';
+    });
+
     const navbarObjectArray = [
         { id: 1, title: 'Blogs' },
         { id: 2, title: 'Uploads' },
@@ -12,122 +15,50 @@ function Navbar({ onPageChange }) {
         { id: 5, title: 'Contact' },
         { id: 6, title: 'signin' },
         { id: 7, title: 'signup' }
-    ]
-
-    console.log('Navbar rendered');
+    ];
 
     useEffect(() => {
-        // Remove the background color from all navbar items
         navbarObjectArray.forEach(item => {
             const element = document.getElementById(`navbarItem${item.id}`);
             if (element) {
-                element.style.backgroundColor = ''; // Reset background color
+                element.style.backgroundColor = ''; 
             }
         });
 
-        // Add background color to the selected navbar item
         const selectedElement = document.getElementById(`navbarItem${navbarObjectArray.find(item => item.title === currentPage).id}`);
         if (selectedElement) {
-            selectedElement.style.backgroundColor = '#5999B1'; // Your desired color
+            selectedElement.style.backgroundColor = '#5999B1'; 
         }
 
+        sessionStorage.setItem('currentPage', currentPage);
+
         console.log("Current page is: ", currentPage);
-    }, [currentPage, navbarObjectArray])
+    }, [currentPage, navbarObjectArray]);
 
     return (
-        <header
-            className={style.navHeader}
-            id='navHeaderId'
-        >
-            <nav
-                className={style.navbar}
-                id='navbarId'
-            >
-                {/* <div 
-                    className={style.navbarLogo} 
-                    id='navbarLogoId'
-                    >
-                    <button 
-                        className={style.logoButton} 
-                        id='logoButtonId'
-                        onClick={()=>onPageChange('Blogs')}
-                    >
-                        <img 
-                            alt="logo" 
-                            id='logoId'
-                            src="images/logo1.png" 
-                        />
-                        <span 
-                            className={style.CodeBlogName}
-                            id='CodeBlogNameId' 
-                        >
-                            CodeBlog
-                        </span>
-                    </button>
-                </div> */}
-                <div
-                    className={style.navbarOption}
-                    id='navbarOptionId'
-                >
-                    <ul
-                        className={style.navbarList}
-                        id='navbarListId'
-                    >
+        <header className={style.navHeader} id='navHeaderId'>
+            <nav className={style.navbar} id='navbarId'>
+                <div className={style.navbarOption} id='navbarOptionId'>
+                    <ul className={style.navbarList} id='navbarListId'>
                         {navbarObjectArray.map((item) => {
                             return (
-                                <li
-                                    className={style.navbarItem}
-                                    id={`navbarItem${item.id}`}
-                                    key={item.id}
-                                >
+                                <li className={style.navbarItem} id={`navbarItem${item.id}`} key={item.id}>
                                     <Link
                                         className={style.navbarLinkButton}
                                         id={`navbarLinkButton${item.id}`}
-                                        // onClick={()=>onPageChange(item.title)} 
                                         onClick={() => { setCurrentPage(item.title) }}
-
                                         to={`/${item.title}`}
-                                    >{item.title}
+                                    >
+                                        {item.title}
                                     </Link>
                                 </li>
                             )
                         })}
                     </ul>
                 </div>
-                {/* <div 
-                    className={style.navbarRight} 
-                    id='navbarRightId'
-                >
-                    <button 
-                        className={style.notificationButton}
-                        id='notificationButtonId'
-                    >
-                        <img 
-                            alt="Notification" 
-                            id='notificationLogoId'
-                            src="images/notification.png" 
-                        />
-                    </button>
-                    <div 
-                        className={style.navbarProfile} 
-                        id='navbarProfileId'
-                        >
-                        <button 
-                            className={style.profileButton} 
-                            id='profileButtonId'
-                            onClick={()=>onPageChange('Profile')}
-                        >
-                                <img 
-                                    alt="Profile" 
-                                    id='profileLogoId'
-                                    src="images/profile-photo.png" 
-                                />
-                        </button>
-                    </div>
-                </div> */}
             </nav>
         </header>
     )
 }
 
-export default Navbar
+export default Navbar;
