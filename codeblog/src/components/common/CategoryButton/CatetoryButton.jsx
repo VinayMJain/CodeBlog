f5f1d50bdd05d774c85329e36c9d55dc99ae693f
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-06-23 01:43:56 +0530

    fix: CatetoryButton

--- codeblog/src/components/common/CategoryButton/CatetoryButton.jsx
+++ codeblog/src/components/common/CategoryButton/CatetoryButton.jsx
@@ -1,28 +1,57 @@
 import React from 'react'
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import blogs from '../../../blogs.json';
 
 export const CatetoryButton = () => {
+  const [alignment, setAlignment] = React.useState('web');
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
   return (
-    <ButtonGroup 
-      variant="contained" 
-      aria-label="Basic button group"
-      sx={{
-        marginLeft: '30px',
-        marginTop: '20px',
-        backgroundColor: 'white',
-        color: 'black',
-        "button": {
-          backgroundColor: "#224452"
-        },
-        // TODO: Add hover effect & Background color change to white
-      }}
-    >
-      <Button>ALL</Button>
-      {blogs.map((blog, index) => (
-        <Button key={index}>{blog.category}</Button>
-      ))}
-    </ButtonGroup>
+    <ToggleButtonGroup
+    color="primary"
+    value={alignment}
+    exclusive
+    onChange={handleChange}
+    aria-label="Platform"
+    sx={{
+      marginLeft: '30px',
+      marginTop: '20px',
+      backgroundColor: 'white',
+      color: "white",
+      "button": {
+        backgroundColor: "#508ba3"
+      },
+    }}
+  >
+    <ToggleButton value="all">All</ToggleButton>
+    {blogs.map((blog, index) => (
+      <ToggleButton key={index} value={blog.category}>{blog.category}</ToggleButton>
+    ))}
+  </ToggleButtonGroup>
   )
 }
+
+
+// <ButtonGroup 
+// variant="contained" 
+// aria-label="Basic button group"
+// sx={{
+//   marginLeft: '30px',
+//   marginTop: '20px',
+//   backgroundColor: 'white',
+//   color: 'black',
+//   "button": {
+//     backgroundColor: "#224452"
+//   },
+//   // TODO: Add hover effect & Background color change to white
+// }}
+// >
+// <Button>ALL</Button>
+// {blogs.map((blog, index) => (
+//   <Button key={index}>{blog.category}</Button>
+// ))}
+// </ButtonGroup>
